7440283eb889e478cc7d3d3e0efcd8a935887f6a
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-02-03 17:07:28 -0300

    initial configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const server = express();
 
-server.get("/", (req, res) => {
-
-
-  return res.send("Olá, DEV!");
+server.get("/", (_, res) => {
+     return res.send("Olá, DEV!");
 });
 
-
-export {server}; 
+export { server };
